537c7a4c319d8852b9de2719d8da18e5b255e0bf
Jack Laverty <jacktlaverty@gmail.com>
2023-03-09 00:36:18 +1100

    mongodb trial

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 
-import redis
 import arinc424.record as a424
+from pymongo import MongoClient, InsertOne
+import json
 
 if __name__ == '__main__':
-    db = redis.Redis(host='localhost', port=6379, db=0)
-
-    file = './input/airport'
-    with open(file) as f:
+    client = MongoClient('mongodb://192.168.20.3:27017/')
+    records = []
+    with open('./input/airport') as f:
         for line in f.readlines():
-            q = 0
             r = a424.Record()
             if r.validate(line):
                 if r.read(line):
-                    q += 1
-                    db.set(q, r.json(False))
+                    print(type(json.loads(r.json())))
+                    records.append(InsertOne(json.loads(r.json())))
+    print('db write:', client.database.collection.bulk_write(records))
+    client.close()
